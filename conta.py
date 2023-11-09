f37db69bfb078a4ddfa17899d83d738f612b70d9
Eduardo Siqueira <53954835+DukaSiqueira@users.noreply.github.com>
2023-11-09 11:00:51 -0300

    Tornando atributos privados

--- conta.py
+++ conta.py
@@ -2,17 +2,19 @@ class Conta:
     # Função construtora
     # Self é a referência para encontrar o objeto em memória
     def __init__(self, numero, titular, saldo, limite):
-        print(self)
-        self.numero = numero
-        self.titular = titular
-        self.saldo = saldo
-        self.limite = limite
+        # O "__" define que o atributo é privado. O Python apenas o identifica e o
+        # desenvolvedor por conciência deve saber que o atributo idenficado não
+        # deve ser usado fora da classe.
+        self.__numero = numero
+        self.__titular = titular
+        self.__saldo = saldo
+        self.__limite = limite
 
     def extrato(self):
-        print("Saldo {} titular {}".format(self.saldo, self.titular))
+        print("Saldo {} titular {}".format(self.__saldo, self.__titular))
 
     def depositar(self, valor):
-        self.saldo += valor
+        self.__saldo += valor
 
     def sacar(self, valor):
-        self.saldo -= valor
+        self.__saldo -= valor
